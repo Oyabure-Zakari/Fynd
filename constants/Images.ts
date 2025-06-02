b7e009d1d9d121b4f69e4f373575962e1020a1f5
Oyabure Zakari <oyabure205@gmail.com>
2025-06-02 13:45:27 +0100

    create images object

---
+++ constants/Images.ts
@@ -0,0 +1,8 @@
+export const IMAGES = {
+  onboard1: require("../assets/images/onboarding1.png"),
+  onboard2: require("../assets/images/onboarding2.png"),
+  onboard3: require("../assets/images/onboarding3.png"),
+  onboard4: require("../assets/images/onboarding4.png"),
+  user: require("../assets/images/user.png"),
+  admin: require("../assets/images/admin.png"),
+}
