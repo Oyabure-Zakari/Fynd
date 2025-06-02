d7df45927c58848941e23c107f408bbabedcdace
Oyabure Zakari <oyabure205@gmail.com>
2025-06-02 13:06:42 +0100

    create layout and index file

---
+++ app/(onboarding)/index.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function OnboardingScreen() {
+  return (
+
+    <View style={styles.container}>
+      <Text> Onboarding Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
