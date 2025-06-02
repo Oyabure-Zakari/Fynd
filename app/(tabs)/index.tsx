8fedee6acf47dc48ac9fc320373e2ce85fb4b643
Oyabure Zakari <oyabure205@gmail.com>
2025-06-02 17:37:45 +0100

    implement bottom sheet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import CustomBottomSheet from "@/components/home/CustomButtomSheet";
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "../styles/home.styles";
 
 export default function Home() {
   return (
-
     <View style={styles.container}>
       <Text> Home Screen</Text>
+      <CustomBottomSheet />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
