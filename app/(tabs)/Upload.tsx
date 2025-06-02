86fe8655f2df381a74cf4e0fea9d7fc5bb079402
Oyabure Zakari <oyabure205@gmail.com>
2025-06-02 16:25:50 +0100

    create upload tabbar

---
+++ app/(tabs)/Upload.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Upload() {
+  return (
+    <View style={styles.container}>
+      <Text>Upload</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
