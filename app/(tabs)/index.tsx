4deead1690963249e360bf6858c51ebd185d42ce
Oyabure Zakari <oyabure205@gmail.com>
2025-06-03 21:42:44 +0100

    update the importation of the home.styles file

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import CustomBottomSheet from "@/components/home/CustomButtomSheet";
 import { useRouter } from "expo-router";
 import React from "react";
 import { Button, Text, View } from "react-native";
-import styles from "../styles/home.styles";
+import styles from "../../styles/home.styles";
 
 export default function Home() {
   const router = useRouter();
