b98e83c68058d505171a078cca928f0cbed783b9
Oyabure Zakari <oyabure205@gmail.com>
2025-06-03 16:32:49 +0100

    create component for onboarding buttons

---
+++ components/onboarding/OnboardingButtons.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import { COLORS } from "@/constants/Colors";
+import { FONTSIZE } from "@/constants/FontSize";
+import { useRouter } from "expo-router";
+import { Button } from "react-native-elements";
+
+export const skipButton = () => {
+  const router = useRouter();
+  return (
+    <View style={styles.button}>
+      <Button
+        title="Skip"
+        buttonStyle={{ backgroundColor: COLORS.white, borderRadius: 10 }}
+        titleStyle={{
+          color: COLORS.blackGrey,
+          fontSize: FONTSIZE.secondaryFont.small,
+          fontFamily: "secondaryFont",
+          padding: 8,
+        }}
+        onPress={() => router.push("/(tabs)")}
+      />
+    </View>
+  );
+};
+
+export const nextButton = ({ ...props }) => {
+  return (
+    <View style={styles.button}>
+      <Button
+        title="Next"
+        buttonStyle={{ backgroundColor: COLORS.purple, borderRadius: 10, width: 60 }}
+        titleStyle={{
+          color: COLORS.white,
+          fontFamily: "segoeui_blackItalic",
+          padding: 2,
+          fontSize: FONTSIZE.secondaryFont.small,
+        }}
+        {...props}
+      />
+    </View>
+  );
+};
+
+export const doneButton = () => {
+  const router = useRouter();
+  return (
+    <View style={styles.button}>
+      <Button
+        title="Get Started"
+        buttonStyle={{ backgroundColor: COLORS.purple, borderRadius: 10}}
+        titleStyle={{
+          color: COLORS.white,
+          fontFamily: "segoeui_blackItalic",
+          padding: 2,
+          fontSize: FONTSIZE.secondaryFont.small,
+        }}
+        onPress={() => router.push("/(tabs)")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    paddingRight: 16,
+  },
+});
