d4adff7dbd6d65489722d3e952560449e44b39aa
Oyabure Zakari <oyabure205@gmail.com>
2025-06-03 14:56:54 +0100

    add button to navigate to onboarding screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
 import CustomBottomSheet from "@/components/home/CustomButtomSheet";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import styles from "../styles/home.styles";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <View style={styles.container}>
       <Text> Home Screen</Text>
+      <Button title="Click" onPress={() => router.push("/(onboarding)")}/>
       <CustomBottomSheet />
     </View>
   );
