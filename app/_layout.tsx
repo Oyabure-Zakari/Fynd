9f713e4d2c1754674a2f348a59f5285f33a37603
Oyabure Zakari <oyabure205@gmail.com>
2025-06-05 21:29:46 +0100

    had status bar background color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
 import SafeScreen from '@/components/SafeScreen';
+import { COLORS } from '@/constants/Colors';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -19,7 +21,8 @@ export default function RootLayout() {
 
   return (
     <SafeAreaProvider>
-      <StatusBar style="light" />
+      <View style={{ height: 40, backgroundColor: COLORS.white, position: 'absolute', top: 0, left: 0, right: 0, zIndex: 1 }} />
+      <StatusBar style="dark"/>
       <SafeScreen>
         <GestureHandlerRootView style={{ flex: 1 }}>
           <Stack>
