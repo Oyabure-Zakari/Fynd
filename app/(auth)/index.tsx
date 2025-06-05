644b7efd335b770f383c2e382e94c2672c92bc30
Oyabure Zakari <oyabure205@gmail.com>
2025-06-05 16:28:58 +0100

    create auth folder

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,68 @@
+import Admin from "@/components/auth/Admin";
+import User from "@/components/auth/User";
+import { COLORS } from "@/constants/Colors";
+import { FONTSIZE } from "@/constants/FontSize";
+import { Picker } from "@react-native-picker/picker";
+import React, { useState } from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+export default function index() {
+  const [selectedUserType, setSelectedUserType] = useState("user");
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <Text style={styles.title}>Select User Type</Text>
+
+        <Picker
+          selectedValue={selectedUserType}
+          onValueChange={(itemValue) => setSelectedUserType(itemValue)}
+          mode="dropdown"
+          style={styles.picker}
+        >
+          <Picker.Item label="User" value="user" />
+          <Picker.Item label="Admin" value="admin" />
+        </Picker>
+      </View>
+
+      <View style={styles.bottomContainer}>
+        <ScrollView>
+          {selectedUserType === "user" ? <User /> : <Admin/>}
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+
+  topContainer: {
+    height: Dimensions.get("window").height * 0.2,
+    width: Dimensions.get("window").width * 0.9,
+    gap: 30,
+    // backgroundColor: "grey"
+  },
+
+  title: {
+    fontFamily: "primaryFont",
+    fontSize: FONTSIZE.primaryFont.medium,
+    color: COLORS.blackGrey,
+  },
+
+  picker: {
+    backgroundColor: COLORS.white,
+    borderRadius: 10,
+  },
+
+  bottomContainer: {
+    height: Dimensions.get("window").height * 0.8,
+    width: Dimensions.get("window").width * 0.9,
+    // backgroundColor: "pink",
+    alignItems: "center",
+  },
+});
