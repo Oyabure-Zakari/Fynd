eccd03375165a7772c4fde39819f9c4331ee9e8e
Oyabure Zakari <oyabure205@gmail.com>
2025-06-06 18:39:02 +0100

    use auth styles instead of styleSheet styles

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,20 +1,23 @@
+import React, { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+
+import { Picker } from "@react-native-picker/picker";
+
 import Admin from "@/components/auth/Admin";
 import User from "@/components/auth/User";
-import { COLORS } from "@/constants/Colors";
-import { FONTSIZE } from "@/constants/FontSize";
-import { Picker } from "@react-native-picker/picker";
-import React, { useState } from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import styles from "@/styles/auth.styles";
 
 export default function index() {
   const [selectedUserType, setSelectedUserType] = useState("user");
 
   return (
     <View style={styles.container}>
+      {/* top container */}
       <View style={styles.topContainer}>
+        {/* selcet user type */}
         <Text style={styles.title}>Select User Type</Text>
 
+        {/* Picker for user type selection */}
         <Picker
           selectedValue={selectedUserType}
           onValueChange={(itemValue) => setSelectedUserType(itemValue)}
@@ -26,48 +29,16 @@ export default function index() {
         </Picker>
       </View>
 
+      {/* bottom container*/}
       <View style={styles.bottomContainer}>
-        <ScrollView 
-          style={{flex:1, width: "100%" }}
+        <ScrollView
+          style={{ flex: 1, width: "100%" }}
           contentContainerStyle={{ flexGrow: 1 }}
           keyboardShouldPersistTaps="handled"
-          >
-          {selectedUserType === "user" ? <User /> : <Admin/>}
+        >
+          {selectedUserType === "user" ? <User /> : <Admin />}
         </ScrollView>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: COLORS.white,
-  },
-
-  topContainer: {
-    height: Dimensions.get("window").height * 0.2,
-    width: Dimensions.get("window").width * 0.9,
-    gap: 30,
-    // backgroundColor: "grey"
-  },
-
-  title: {
-    fontFamily: "primaryFont",
-    fontSize: FONTSIZE.primaryFont.medium,
-    color: COLORS.blackGrey,
-  },
-
-  picker: {
-    backgroundColor: COLORS.white,
-    borderRadius: 10,
-  },
-
-  bottomContainer: {
-    height: Dimensions.get("window").height * 0.8,
-    width: Dimensions.get("window").width * 0.9,
-    // backgroundColor: "pink",
-    alignItems: "center",
-  },
-});
