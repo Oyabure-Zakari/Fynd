a2ec2e4c2ebcb06f0586ec876f96c4ebddb346fa
Oyabure Zakari <oyabure205@gmail.com>
2025-06-06 18:36:24 +0100

    create folder to house all reuseable styles

---
+++ styles/reusableStyles/googleButton.styles.ts
@@ -0,0 +1,30 @@
+import { COLORS } from "@/constants/Colors";
+import { FONTSIZE } from "@/constants/FontSize";
+import { Dimensions, StyleSheet } from "react-native";
+
+const googleButtonStyles = StyleSheet.create({
+  googleButton: {
+    paddingVertical: 10,
+    backgroundColor: COLORS.purple,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    width: Dimensions.get("window").width * 0.9, // 90% of screen width
+    gap: 20,
+  },
+
+  googleImage: {
+    width: 30,
+    height: 30,
+    resizeMode: "contain",
+  },
+
+  googleButtonText: {
+    fontFamily: "secondaryFont",
+    color: COLORS.primaryGrey,
+    fontSize: FONTSIZE.secondaryFont.small,
+  },
+});
+
+export default googleButtonStyles;
