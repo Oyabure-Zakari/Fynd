78dcce0cf0c0658778d4ddfe5d91abeace3672e8
Oyabure Zakari <oyabure205@gmail.com>
2025-06-08 23:24:54 +0100

    add remove applaunch token function

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,11 +6,14 @@ import styles from "../../styles/home.styles";
 
 export default function Home() {
   const router = useRouter();
+  // const removeAppLaunch = useAppLaunchedStore((state) => state.removeAppLaunch);
+
   return (
     <View style={styles.container}>
       <Text> Home Screen</Text>
       <Button title="Click" onPress={() => router.push("/(onboarding)")}/>
       <Button title="auth" onPress={() => router.push("/(auth)")}/>
+      {/* <Button title="remove" onPress={removeAppLaunch}/> */}
       <CustomBottomSheet />
     </View>
   );
