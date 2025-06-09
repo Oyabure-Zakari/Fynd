1183cac41041162137694ff03b47c412546690ef
Oyabure Zakari <oyabure205@gmail.com>
2025-06-09 11:37:34 +0100

    implement code to check f the app has already launched, if true returns null

--- app/(onboarding)/index.tsx
+++ app/(onboarding)/index.tsx
@@ -1,7 +1,11 @@
 import OnboardingUI from "@/components/onboarding/OnboardingUI";
+import { useAppLaunchedStore } from "@/store/appLaunched";
 import React from "react";
 
 export default function index() {
+  const isCheckingLaunch = useAppLaunchedStore((state) => state.isCheckingLaunch); 
+  
+  if (isCheckingLaunch) return null; // if the app has already launched, redirect to the auth screen
   return (
     <OnboardingUI/>
   );
