d5020118a1df783e8a91c3c58cc5e136a60a08e2
Oyabure Zakari <oyabure205@gmail.com>
2025-06-09 11:50:18 +0100

    add logout admin functionality

--- app/(tabs)/Upload.tsx
+++ app/(tabs)/Upload.tsx
@@ -1,11 +1,23 @@
-import { COLORS } from '@/constants/Colors'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { COLORS } from '@/constants/Colors';
+import { useLoginAdminStore } from "@/store/loginAdmin";
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function Upload() {
+  const router = useRouter();
+    // const removeAppLaunch = useAppLaunchedStore((state) => state.removeAppLaunch);
+  const logoutAdmin = useLoginAdminStore((state) => state.logoutAdmin);
+
+  const handleLogoutAdmin = () => {
+    logoutAdmin();
+    router.push("/(tabs)");
+  }
+
   return (
     <View style={styles.container}>
       <Text>Upload</Text>
+      <Button title="logoutAdmin" onPress={handleLogoutAdmin}/>
     </View>
   )
 }
