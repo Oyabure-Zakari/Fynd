da79197b937f9662e2ed3183dfd1369bdafe77ec
Oyabure Zakari <oyabure205@gmail.com>
2025-06-13 05:13:38 +0100

    improve navigation logic code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,11 @@
 import SafeScreen from "@/components/SafeScreen";
 import { COLORS } from "@/constants/Colors";
 import { useAppLaunchedStore } from "@/store/appLaunched";
+import { useGoogleSignInStore } from "@/store/googleSignIn";
+import {
+  appLaunchedNavigationLogic,
+  googleSignInNavigationLogic,
+} from "@/utils/navigationLogic";
 
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack, useRouter, useSegments } from "expo-router";
@@ -17,8 +22,13 @@ export default function RootLayout() {
   const router = useRouter();
   const segments = useSegments();
 
+  // app launch
   const launched = useAppLaunchedStore((state) => state.appLaunched);
   const checkAppLaunch = useAppLaunchedStore((state) => state.checkAppLaunch);
+
+  // google signin
+  const idToken = useGoogleSignInStore((state) => state.idToken);
+  const checkIdToken = useGoogleSignInStore((state) => state.checkIdToken);
 
   const [loaded] = useFonts({
     primaryFont: require("../assets/fonts/CoolveticaRg-Regular.ttf"),
@@ -26,25 +36,23 @@ export default function RootLayout() {
   });
 
   useEffect(() => {
-    // updates the appLaunched state immediately the app is launched
+    // checks if app has launched before
     checkAppLaunch();
+    // checks if google id token is set
+    checkIdToken();
   }, []);
 
   useEffect(() => {
     if (loaded) {
       // Only navigate after fonts are loaded and Stack is rendered
-      const hasLaunchedApp = launched;
-      const isInOnboardingScreen = segments[0] === "(onboarding)";
-
-      if (!hasLaunchedApp && !isInOnboardingScreen)
-        router.replace("/(onboarding)");
-      else if (hasLaunchedApp && isInOnboardingScreen) router.replace("/(auth)");
+      appLaunchedNavigationLogic(launched, router, segments); // function that handles app launched navigation logic
+      googleSignInNavigationLogic(idToken, router, segments); // funcion that handles google SsignIn navigation logic
 
       SplashScreen.hideAsync();
     }
-  }, [loaded, launched, segments]);
+  }, [loaded, launched, idToken, segments]);
 
-    if (!loaded) {
+  if (!loaded) {
     // Aysnc font loading is still in progress
     return null;
   }
@@ -55,9 +63,9 @@ export default function RootLayout() {
       <SafeScreen>
         <GestureHandlerRootView style={{ flex: 1 }}>
           <Stack>
-            <Stack.Screen
-              name="(tabs)"
-              options={{ headerShown: false }}
+            <Stack.Screen 
+              name="(tabs)" 
+              options={{ headerShown: false }} 
             />
 
             <Stack.Screen
@@ -65,9 +73,9 @@ export default function RootLayout() {
               options={{ headerShown: false }}
             />
 
-            <Stack.Screen
-              name="(auth)"
-              options={{ headerShown: false }}
+            <Stack.Screen 
+              name="(auth)" 
+              options={{ headerShown: false }} 
             />
           </Stack>
         </GestureHandlerRootView>
