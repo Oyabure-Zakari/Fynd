34681a0688de70561f28a21f120ac64c89111f76
Oyabure Zakari <oyabure205@gmail.com>
2025-06-13 02:07:53 +0100

    add web and andriod cient id

--- env.d.ts
+++ env.d.ts
@@ -2,4 +2,7 @@ declare module '@env' {
   export const ADMIN_TOKEN: string;
   export const ADMIN_USERNAME: string;
   export const ADMIN_PASSWORD: string;
+
+  export const WEB_CLIENT_ID: string;
+  export const ANDRIOD_CLIENT_ID: string;
 }
