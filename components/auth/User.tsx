d2c0bde4ac872fa333c142bc95ed1e65e9ab973c
Oyabure Zakari <oyabure205@gmail.com>
2025-06-13 02:12:02 +0100

    remove google config object

--- components/auth/User.tsx
+++ components/auth/User.tsx
@@ -14,7 +14,7 @@ export default function User() {
       {/* user type text */}
       <Text style={styles.userWriteUp}>
         As an ordinary user, explore the world with ease! Use our interactive
-        map to find your location and search for destinations.{" "}
+        map to find your location and search for destinations.
       </Text>
 
       {/* Sign in with Google button */}
@@ -22,3 +22,4 @@ export default function User() {
     </View>
   );
 }
+
